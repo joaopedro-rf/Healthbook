bab411317d4c8a6c4214416db894e73085704439
Joao Pedro <joaopedro-rf@Hotmail.com>
2023-10-07 16:11:49 -0300

    first commit - Initial configuration

---
+++ Healthbook/src/main/java/com/example/Healthbook/HealthbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.Healthbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthbookApplication.class, args);
+	}
+
+}

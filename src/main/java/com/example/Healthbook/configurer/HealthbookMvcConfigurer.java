cd2b5a597adf9e246cb0c5eca038567cdc607be8
joaopedro-rf <42155548+joaopedro-rf@users.noreply.github.com>
2024-01-23 16:04:49 -0300

    Update HealthbookMvcConfigurer.java

--- src/main/java/com/example/Healthbook/configurer/HealthbookMvcConfigurer.java
+++ src/main/java/com/example/Healthbook/configurer/HealthbookMvcConfigurer.java
@@ -23,7 +23,7 @@ public class HealthbookMvcConfigurer implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api")
                 .allowedOrigins("http://127.0.0.1:5500")
-                .allowedOrigins("https://healthclinic.vercel.app")
+                .allowedOrigins("https://healthclinic.vercel.app/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
